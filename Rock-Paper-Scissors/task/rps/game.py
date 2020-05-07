db9c9c735878daa57b64cf324039c41a33ef2e37
muhammed-farghali <muhammad.farghali14@gmail.com>
2020-05-07 17:16:55 +0200

    Finish Stage#3

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,32 +1,24 @@
 import random
 
-options = ('rock', 'paper', 'scissors')
+options = ('rock', 'scissors', 'paper')
 
 
 def winner(u_choice, c_choice):
-    if u_choice == 'rock':
-        if c_choice == 'paper':
-            print(f'Sorry, but computer chose {c_choice}')
-        elif c_choice == 'rock':
-            print(f'There is a draw ({c_choice})')
-        elif c_choice == 'scissors':
-            print(f'Well done. Computer chose {c_choice} and failed')
-    elif u_choice == 'paper':
-        if c_choice == 'paper':
-            print(f'There is a draw ({c_choice})')
-        elif c_choice == 'rock':
-            print(f'Well done. Computer chose {c_choice} and failed')
-        elif c_choice == 'scissors':
-            print(f'Sorry, but computer chose {c_choice}')
-    elif u_choice == 'scissors':
-        if c_choice == 'paper':
-            print(f'Well done. Computer chose {c_choice} and failed')
-        elif c_choice == 'rock':
-            print(f'Sorry, but computer chose {c_choice}')
-        elif c_choice == 'scissors':
-            print(f'There is a draw ({c_choice})')
+    if c_choice == u_choice:
+        print(f'There is a draw ({c_choice})')
+    elif c_choice == options[options.index(u_choice) - 1]:
+        print(f'Sorry, but computer chose {c_choice}')
+    elif c_choice == options[options.index(u_choice) - 2]:
+        print(f'Well done. Computer chose {c_choice} and failed')
 
 
-winner(input().strip(), random.choice(options))
-
-
+while True:
+    u_inp = input().strip()
+    if u_inp == '!exit':
+        print('Bye!')
+        break
+    elif u_inp not in options:
+        print('Invalid input')
+        continue
+    else:
+        winner(u_inp, random.choice(options))
