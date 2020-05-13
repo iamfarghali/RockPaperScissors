97f05b6e166bfb6ad9b07b2c3a71add5cb71f27c
muhammed-farghali <muhammad.farghali14@gmail.com>
2020-05-13 17:20:38 +0200

    Finish Stage #5 [Rock-Paper-Scissors] Done!

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,31 +1,56 @@
 import random
-
 options = ('rock', 'scissors', 'paper')
-current_score = 0
 
 
-def winner(u_choice, c_choice):
+def get_current_score(u_name):
+    players_scores = {}
+    rating_file = open('rating.txt', 'r')
+    for line in rating_file:
+        p_info = line.split()
+        players_scores[p_info[0]] = p_info[1]
+    return int(players_scores[username]) if username in players_scores else 0
+
+
+def winner(uc, cc, ops):
+    if ops == options:
+        if uc == cc:
+            return 'draw'
+        return 'c_won' if cc == ops[ops.index(uc) - 1] else 'u_won'
+    else:
+        u_choice_index = 0
+        for i in range(len(ops)):
+            if ops[i] == uc:
+                u_choice_index = i
+                break
+        n_options = [o for i, o in enumerate(ops) if i > u_choice_index]
+        n_options += [o for i, o in enumerate(ops) if i < u_choice_index]
+        strong_half = [o for i, o in enumerate(n_options) if i < (len(n_options) / 2)]
+        weak_half = [o for o in n_options if o not in strong_half]
+        if uc == cc:
+            return 'draw'
+        return 'c_won' if cc in strong_half else 'u_won'
+
+
+def game(u_choice, opts=options):
     global current_score
-    if c_choice == u_choice:
+    c_choice = random.choice(opts)
+    status = winner(u_choice, c_choice, opts)
+    if status == 'draw':
         current_score += 50
         print(f'There is a draw ({c_choice})')
-    elif c_choice == options[options.index(u_choice) - 1]:
+    elif status == 'c_won':
         print(f'Sorry, but computer chose {c_choice}')
-    elif c_choice == options[options.index(u_choice) - 2]:
+    elif status == 'u_won':
         current_score += 100
         print(f'Well done. Computer chose {c_choice} and failed')
 
 
-players_scores = {}
 username = input('Enter your name: ')
+current_score = get_current_score(username)
 print(f'Hello, {username}')
 
-rating_file = open('rating.txt', 'r')
-for line in rating_file:
-    p_info = line.split()
-    players_scores[p_info[0]] = p_info[1]
-
-current_score = players_scores[username] if username in players_scores else 0
+user_options = input().strip().split(',')
+print("Okay, let's start")
 
 while True:
     u_inp = input().strip()
@@ -34,8 +59,8 @@ while True:
         break
     elif u_inp == '!rating':
         print(f'Your rating: {current_score}')
-    elif u_inp not in options:
+    elif u_inp not in options and u_inp not in user_options:
         print('Invalid input')
         continue
     else:
-        winner(u_inp, random.choice(options))
+        game(u_inp) if len(user_options) == 1 else game(u_inp, user_options)
